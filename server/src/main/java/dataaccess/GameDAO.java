075b77192b8de38df02299d7f21d24497a4c8d52
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-10-20 21:43:27 -0600

    Implemented MemoryGameDAO.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,13 @@
 package dataaccess;
 
+import models.GameData;
+
 public interface GameDAO {
+    GameData getGame(int gameID);
+
+    void insertGame(GameData gameData);
+
+    void deleteGame(int gameID) throws DataAccessException;
+
+    void clear();
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,32 @@
 package dataaccess;
 
+import models.GameData;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryGameDAO implements GameDAO{
+    private final Map<Integer, GameData> table = new HashMap<>();
+
+    @Override
+    public GameData getGame(int gameID) {
+        return table.get(gameID);
+    }
+
+    @Override
+    public void insertGame(GameData gameData) {
+        table.put(gameData.gameID(),gameData);
+    }
+
+    @Override
+    public void deleteGame(int gameID) throws DataAccessException {
+        if (table.remove(gameID) == null) {
+            throw new DataAccessException("Auth Token is bad!");
+        }
+    }
+
+    @Override
+    public void clear() {
+        table.clear();
+    }
 }
