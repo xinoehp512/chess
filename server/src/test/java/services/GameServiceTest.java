12a75c649d394a20cb0c791415edf2f746fb6bd8
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-10-21 17:35:49 -0600

    Added Join Game White and Join Game Black service tests

--- server/src/test/java/services/GameServiceTest.java
+++ server/src/test/java/services/GameServiceTest.java
@@ -5,17 +5,18 @@ import models.AuthData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import requests.CreateGameRequest;
+import requests.JoinGameRequest;
 import requests.RegisterRequest;
 import requests.ResponseException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GameServiceTest {
     GameService gameService;
     GameDAO gameDAO;
     AuthDAO authDAO;
     private AuthData authData;
+    private AuthData authDataOtherUser;
 
     @BeforeEach
     void initGameService() throws ResponseException {
@@ -24,6 +25,7 @@ class GameServiceTest {
         gameService = new GameService(gameDAO, authDAO);
         var userService = new UserService(new MemoryUserDAO(), authDAO);
         authData = userService.register(new RegisterRequest("xinoehp512", "password", "e@e.com"));
+        authDataOtherUser = userService.register(new RegisterRequest("Brain", "zzzzz", "z@z.com"));
     }
 
     @Test
@@ -38,4 +40,29 @@ class GameServiceTest {
         assertEquals(gameName, gameDAO.getGame(response.gameID()).gameName());
     }
 
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void joinGameWhite() throws ResponseException {
+        String gameName = "Game 1";
+        var response = gameService.createGame(new CreateGameRequest(gameName), authData.authToken());
+        var gameID = response.gameID();
+        gameService.joinGame(new JoinGameRequest("WHITE", gameID), authData.authToken());
+        var game = gameDAO.getGame(gameID);
+        assertNull(game.blackUsername());
+        assertEquals(authData.username(), game.whiteUsername());
+    }
+
+    @Test
+    void joinGameBlack() throws ResponseException {
+        String gameName = "Game 1";
+        var response = gameService.createGame(new CreateGameRequest(gameName), authData.authToken());
+        var gameID = response.gameID();
+        gameService.joinGame(new JoinGameRequest("BLACK", gameID), authData.authToken());
+        var game = gameDAO.getGame(gameID);
+        assertNull(game.whiteUsername());
+        assertEquals(authData.username(), game.blackUsername());
+    }
 }
