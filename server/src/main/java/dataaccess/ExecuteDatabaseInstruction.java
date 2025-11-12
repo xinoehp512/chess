ea411e4ee81a19e7da1fcc5c7bbed05764626bb8
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-11-11 18:06:01 -0700

    Format in ExecuteDatabaseInstruction.java

--- server/src/main/java/dataaccess/ExecuteDatabaseInstruction.java
+++ server/src/main/java/dataaccess/ExecuteDatabaseInstruction.java
@@ -11,26 +11,26 @@ import static java.sql.Types.NULL;
 
 public class ExecuteDatabaseInstruction {
     public static int executeUpdate(String statement, Object... params) throws DataAccessException {
-        try (Connection conn = DatabaseManager.getConnection()) {
-            try (var preparedStatement = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (int i = 0; i < params.length; i++) {
-                    Object param = params[i];
-                    switch (param) {
-                        case String p -> preparedStatement.setString(i + 1, p);
-                        case Integer p -> preparedStatement.setInt(i + 1, p);
-                        case Object p -> preparedStatement.setString(i + 1, new Gson().toJson(p));
-                        case null -> preparedStatement.setNull(i + 1, NULL);
-                    }
+        try (Connection conn = DatabaseManager.getConnection(); var preparedStatement =
+                conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+            for (int i = 0; i < params.length; i++) {
+                Object param = params[i];
+                switch (param) {
+                    case String p -> preparedStatement.setString(i + 1, p);
+                    case Integer p -> preparedStatement.setInt(i + 1, p);
+                    case Object p -> preparedStatement.setString(i + 1, new Gson().toJson(p));
+                    case null -> preparedStatement.setNull(i + 1, NULL);
                 }
-                preparedStatement.executeUpdate();
-
-                ResultSet rs = preparedStatement.getGeneratedKeys();
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-
-                return 0;
             }
+            preparedStatement.executeUpdate();
+
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+
+            return 0;
+
         } catch (SQLException e) {
             throw new DataAccessException("Database Error: ", e);
         }
