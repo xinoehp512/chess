1a6e2e05d8d654708c40c04bb3f72c53ba931503
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-11-13 13:11:53 -0700

    Added HTTP request builder functions.

--- client/src/main/java/server/ServerFacade.java
+++ client/src/main/java/server/ServerFacade.java
@@ -1,5 +1,7 @@
 package server;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
 import models.AuthData;
 import models.GameData;
 import requests.*;
@@ -7,7 +9,10 @@ import response.CreateGameResponse;
 import response.ListGamesResponse;
 import response.LoginResponse;
 
-import java.net.http.HttpClient;
+import java.net.URI;
+import java.net.http.*;
+import java.net.http.HttpRequest.*;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
@@ -48,5 +53,68 @@ public class ServerFacade {
 
     public GameData getGame(int gameID) {
         return null;
+    }
+
+
+    private HttpRequest buildRequest(String method, String path) {
+        return buildRequest(method, path, null, null);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Request body) {
+        return buildRequest(method, path, body, null);
+    }
+
+    private HttpRequest buildRequest(String method, String path, String authToken) {
+        return buildRequest(method, path, null, authToken);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Request body, String authToken) {
+        var request = HttpRequest.newBuilder().uri(URI.create(
+                serverUrl + path)).method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        if (authToken != null) {
+            request.setHeader("authorization", authToken);
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Request request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(ex.getMessage(), 500);
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException("Other failure: " + status, status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
     }
 }
--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -2,6 +2,7 @@ package exception;
 
 import com.google.gson.Gson;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseException extends Exception {
@@ -10,6 +11,13 @@ public class ResponseException extends Exception {
     public ResponseException(String message, int statusCode) {
         super(message);
         this.statusCode = statusCode;
+    }
+
+    public static ResponseException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        int status = (int) map.get("status");
+        String message = map.get("message").toString();
+        return new ResponseException(message, status);
     }
 
     public int getStatusCode() {
