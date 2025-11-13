358c97444b374736a953a99406fbc96d29e9a833
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-11-13 11:46:45 -0700

    Added ServerFacade stubs

--- client/src/main/java/server/ServerFacade.java
+++ client/src/main/java/server/ServerFacade.java
@@ -1,6 +1,8 @@
 package server;
 
-import requests.LoginRequest;
+import requests.*;
+import response.CreateGameResponse;
+import response.ListGamesResponse;
 import response.LoginResponse;
 
 import java.net.http.HttpClient;
@@ -15,6 +17,26 @@ public class ServerFacade {
 
     public LoginResponse login(LoginRequest loginRequest) {
         return null;
+    }
+
+    public LoginResponse register(RegisterRequest registerRequest) {
+        return null;
+    }
+
+    public void logout(LogoutRequest logoutRequest) {
+
+    }
+
+    public CreateGameResponse createGame(CreateGameRequest createGameRequest) {
+        return null;
+    }
+
+    public ListGamesResponse listGames() {
+        return null;
+    }
+
+    public void joinGame(JoinGameRequest joinGameRequest) {
+
     }
 
 
