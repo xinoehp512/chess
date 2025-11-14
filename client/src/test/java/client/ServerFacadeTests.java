f238f38286dfa9d8ec8f7bc72c3d42d249c1e8f0
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-11-13 18:34:17 -0700

    Un-hardcoded the port.

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -23,9 +23,9 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-        serverFacade = new ServerFacade("http://localhost:8080");
+        serverFacade = new ServerFacade("http://localhost:" + Integer.toString(port));
     }
 
     @AfterAll
